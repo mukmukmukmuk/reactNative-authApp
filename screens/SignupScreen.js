2471a633ea705f0b9bf0562fdc6b41b753ca7435
mukmukmukmuk <ksm010825@naver.com>
2024-04-21 20:32:05 +0900

    섹션 11 중간

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,27 @@
-import AuthContent from '../components/Auth/AuthContent';
+import { useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const signupHandler = async ({ email, password }) => {
+    setIsAuthenticating(true);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not create user, please check your input and try again later."
+      );
+    }
+    setIsAuthenticating(false);
+  };
+  if (isAuthenticating) {
+    return <LoadingOverlay message={"Creating user..."} />;
+  }
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
---
+++ util/auth.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+const API_KEY = "AIzaSyCYTNuqGKOSiRo3cljuqTMDZD4bn51HQiw";
+const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+  console.log(response.data);
+};
+
+export const createUser = async (email, password) => {
+  await authenticate("signUp", email, password);
+};
+
+export const login = async (email, password) => {
+  await authenticate("signInWithPassword", email, password);
+};
